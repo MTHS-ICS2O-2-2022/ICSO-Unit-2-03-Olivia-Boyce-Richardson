efa10569ce7454e1581b8bdb2b37e75fe5586d71
Olivia-Boyce-Richardson <124373077+Olivia-Boyce-Richardson@users.noreply.github.com>
2023-04-04 14:39:11 +0000

    html fixes

--- js/script.js
+++ js/script.js
@@ -3,3 +3,12 @@
 // Created by: Mr. Coxall
 // Created on: Sep 2020
 // This file contains the JS functions for index.html
+function enterClicked() {
+  // input
+  const streetNumber = document.getElementById("street-number").value
+  const streetName = document.getElementById("street-name").value
+
+  // output
+  document.getElementById("address").innerHTML =
+    "Your info is: " + streetNumber + ", " + streetName + "."
+}
